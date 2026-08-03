23f64ca3656eed58bf3d9efe9a49ea081cec523d
Satyam Sandilya <satyam02367@gmail.com>
2026-08-04 01:51:14 +0530

    LC-reverse-integer [Runtime: N/A; Memory: N/A]

---
+++ math/reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int temp = x;
+        long ans = 0;
+
+        while(temp != 0)
+        {
+            int rem = temp%10;
+            ans = ans*10+rem;
+            temp /= 10;
+        }
+
+        return (ans > INT_MAX) ? 0 : (int)ans;
+    }
+};
