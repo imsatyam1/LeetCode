1e9ec483d985133644c759b48a61e9289bffc801
Satyam Sandilya <satyam02367@gmail.com>
2026-08-04 02:00:45 +0530

    LC-palindrome-number [Runtime: N/A; Memory: N/A]

---
+++ math/palindrome-number.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    int reverseNum(int x)
+    {
+        int newNum = 0;
+        
+        while(x != 0)
+        {
+            int rem = x%10;
+            
+            newNum = newNum*10+rem;
+            x /= 10;
+        }
+        return newNum;
+    }
+
+    public bool IsPalindrome(int x) {
+
+        if(x < 0) return false;
+
+        int newNum = reverseNum(x);
+
+        return x == newNum;
+    }
+}
