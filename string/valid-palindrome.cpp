93a4bc4685741282ddb1cc2c630c851dfabce5f2
Satyam Sandilya <satyam02367@gmail.com>
2026-08-05 08:55:12 +0530

    LC-valid-palindrome [Runtime: 0 ms; Memory: 9.9 MB]

--- string/valid-palindrome.cpp
+++ string/valid-palindrome.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    bool helper(string s, int start, int end)
+    bool helper(string &s, int start, int end)
     {
-        while(start < end && !isalnum(s[start])) start++;
-        while(start < end && !isalnum(s[end])) end--;
+        while (start < end && !isalnum(s[start])) start++;
 
-        if(start >=  end) return true;
+        while (start < end && !isalnum(s[end])) end--;
+
+        if(start >= end) return true;
 
         if(tolower(s[start]) != tolower(s[end])) return false;
 
         return helper(s, start+1, end-1);
     }
-
     bool isPalindrome(string s) {
-        int end = s.length() - 1;
+        int end = s.length() -1;
         return helper(s, 0, end);
     }
 };
