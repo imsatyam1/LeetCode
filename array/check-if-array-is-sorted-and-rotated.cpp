b5ae0479eadaf23018579ebfb8142745aa3b2257
Satyam Sandilya <satyam02367@gmail.com>
2026-08-07 12:25:32 +0530

    LC-check-if-array-is-sorted-and-rotated [Runtime: N/A; Memory: N/A]

--- array/check-if-array-is-sorted-and-rotated.cpp
+++ array/check-if-array-is-sorted-and-rotated.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int count = 0;
-        int size = nums.size();
+       int count = 0;
 
-        for(int i=1; i<size; i++)
-        {
-            if(nums[i-1] > nums[i]) count++;
+       for(int i=1; i<nums.size(); i++)
+       {
+            if(nums[i-1] > nums[i])
+            {
+                count++;
 
-            if(count > 1) return false;
-        }
+                if(count > 1) return false;
+            }
 
-        if(count ==1 && nums[0] < nums[size-1]) return false;
-
-        return true;
+            if(count == 1 && nums[0] < nums[nums.size() -1]) return false;
+       }
+       return true;
     }
 };
