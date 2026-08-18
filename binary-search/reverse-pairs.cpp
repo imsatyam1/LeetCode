a1cf014163b6932dc0b378ae4c5d620dbf33d907
Satyam Sandilya <satyam02367@gmail.com>
2026-08-18 12:15:44 +0530

    LC-reverse-pairs [Runtime: N/A; Memory: N/A]

--- binary-search/reverse-pairs.cpp
+++ binary-search/reverse-pairs.cpp
@@ -1,16 +1,87 @@
 class Solution {
-public:
-    int reversePairs(vector<int>& nums) {
-        int n = nums.size();
-        int count = 0;
+private:
+    void merge(vector<int>& nums, int start, int mid, int end)
+    {
+        int left = start;
+        int right = mid + 1;
+        int k = 0;
 
-        for(int i=0; i<n-1; i++)
+        vector<int> temp(end - start + 1);
+
+        while (left <= mid && right <= end)
         {
-            for(int j=i+1; j<n; j++)
+            if (nums[left] <= nums[right])
             {
-                if(nums[i] > 2* nums[j]) count++;
+                temp[k++] = nums[left++];
+            }
+            else
+            {
+                temp[k++] = nums[right++];
             }
         }
+
+        while (left <= mid)
+        {
+            temp[k++] = nums[left++];
+        }
+
+        while (right <= end)
+        {
+            temp[k++] = nums[right++];
+        }
+
+        for (int i = start; i <= end; i++)
+        {
+            nums[i] = temp[i - start];
+        }
+    }
+
+    int countPair(vector<int>& nums, int start, int mid, int end)
+    {
+        int right = mid + 1;
+        int count = 0;
+
+        for (int i = start; i <= mid; i++)
+        {
+            while (right <= end &&
+                   nums[i] > 2LL * nums[right])
+            {
+                right++;
+            }
+
+            count += right - (mid + 1);
+        }
+
         return count;
+    }
+
+    int mergeSort(vector<int>& nums, int start, int end)
+    {
+        if (start >= end)
+            return 0;
+
+        int mid = start + (end - start) / 2;
+
+        int cnt = 0;
+
+        // Sort left half
+        cnt += mergeSort(nums, start, mid);
+
+        // Sort right half
+        cnt += mergeSort(nums, mid + 1, end);
+
+        // Count reverse pairs BEFORE merging
+        cnt += countPair(nums, start, mid, end);
+
+        // Merge both sorted halves
+        merge(nums, start, mid, end);
+
+        return cnt;
+    }
+
+public:
+    int reversePairs(vector<int>& nums)
+    {
+        return mergeSort(nums, 0, nums.size() - 1);
     }
 };
