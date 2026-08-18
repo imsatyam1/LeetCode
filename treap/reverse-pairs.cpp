23c9441d5912e9711454a390d7954cdd63041fa0
Satyam Sandilya <satyam02367@gmail.com>
2026-08-18 11:48:17 +0530

    LC-reverse-pairs [Runtime: N/A; Memory: N/A]

---
+++ treap/reverse-pairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+
+        for(int i=0; i<n-1; i++)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                if(nums[i] > 2* nums[j]) count++;
+            }
+        }
+        return count;
+    }
+};
