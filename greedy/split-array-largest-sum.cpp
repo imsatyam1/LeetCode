1ee381cc3a3ab56e451f003c2aef2e8945c0ec50
Satyam Sandilya <satyam02367@gmail.com>
2026-09-15 20:32:33 +0530

    LC-split-array-largest-sum [Runtime: N/A; Memory: N/A]

---
+++ greedy/split-array-largest-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    bool isPossible(vector<int> &nums, int k, int mid)
+    {
+        int count = 1, sum = 0;
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            if((sum + nums[i]) > mid)
+            {
+                count++;
+                sum = nums[i];
+            }
+            else 
+            {
+                sum += nums[i];
+            }
+        }
+
+        return count <= k;
+    }
+
+public:
+    int splitArray(vector<int>& nums, int k) {
+        int low = 0;
+        int high = accumulate(nums.begin(), nums.end(), 0);
+
+        if(k == 1) return high;
+
+        while(low < high)
+        {
+            int mid = low + (high - low)/2;
+
+            if(isPossible(nums, k, mid)) high = mid;
+            else low = mid + 1;
+        }
+        return high;
+    }
+};
