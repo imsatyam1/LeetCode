4b21119c69841ac078667f72bcd9de9164230b24
Satyam Sandilya <satyam02367@gmail.com>
2026-09-16 15:08:57 +0530

    LC-longest-common-prefix [Runtime: N/A; Memory: N/A]

---
+++ array/longest-common-prefix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n = strs.size();
+        string ans = strs[0];
+
+        if(n == 1) return ans;
+
+        for(string &s: strs)
+        {
+            int i = 0;
+
+            if(s[0] != ans[0]) return "";
+
+            while(s[i] == ans[i]) i++;
+
+            ans = ans.substr(0, i);
+        }
+
+        return ans;
+    }
+};
