b905779cf88b53997efefc448b08797c7185d0fa
Satyam Sandilya <satyam02367@gmail.com>
2026-09-16 15:31:14 +0530

    LC-rotate-string [Runtime: N/A; Memory: N/A]

--- string/rotate-string.cpp
+++ string/rotate-string.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        int i=0;
-
         if(s.length() != goal.length()) return false;
 
-        while(i< s.length() && s[i] != goal[0]) i++;
+        string temp = s+s;
 
-        string newStr = s.substr(i, s.length()) + s.substr(0, i);
-
-        return (newStr == goal);
+        return temp.find(goal) != string::npos;
     }
 };
