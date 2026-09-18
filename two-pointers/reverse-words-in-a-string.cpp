1a124500969dc0babcd156b29b38828b044c41e7
Satyam Sandilya <satyam02367@gmail.com>
2026-09-18 14:36:32 +0530

    LC-reverse-words-in-a-string [Runtime: N/A; Memory: N/A]

--- two-pointers/reverse-words-in-a-string.cpp
+++ two-pointers/reverse-words-in-a-string.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int i = s.size() - 1;
-
+        int n = s.size() - 1;
         string ans = "";
+
+        int i = s.size() -1;
 
         while(i >= 0)
         {
             while(i >= 0 && s[i] == ' ') i--;
 
-            if(i < 0) break;
+            if(i <  0) break;
 
             int j = i;
 
-            while(i >= 0 && s[i] != ' ') i--;
+            while(i>= 0 && s[i] != ' ') i--;
 
-            ans  += s.substr(i+1, j-i);
-
+            ans += s.substr(i+1, j-i);
             ans += ' ';
         }
 
-        return ans.substr(0, ans.size() -1);
+        return ans.substr(0, ans.size() - 1);
     }
 };
